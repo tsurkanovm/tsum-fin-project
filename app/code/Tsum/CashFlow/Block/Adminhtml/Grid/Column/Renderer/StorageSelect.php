36f9c2b627b20969df490ebcb4941dc5d8b331b7
mihail <tsurkanovm@gmail.com>
2020-04-30 11:25:11 +0300

    added correct storage select

--- app/code/Tsum/CashFlow/Block/Adminhtml/Grid/Column/Renderer/StorageSelect.php
+++ app/code/Tsum/CashFlow/Block/Adminhtml/Grid/Column/Renderer/StorageSelect.php
@@ -2,11 +2,34 @@
 
 namespace Tsum\CashFlow\Block\Adminhtml\Grid\Column\Renderer;
 
+use Magento\Backend\Block\Context;
+use Magento\Backend\Block\Widget\Grid\Column\Renderer\Options\Converter;
+use Magento\Backend\Block\Widget\Grid\Column\Renderer\Select;
+use Tsum\CashFlow\Model\Source\CfItem;
+use Tsum\CashFlow\Model\Source\CfItemFactory;
 
-class StorageSelect extends \Magento\Backend\Block\Widget\Grid\Column\Renderer\Select
+class StorageSelect extends Select
 {
+    private $_cfItemSourceFactory;
+
+    public function __construct(
+        Context $context,
+        Converter $converter,
+        CfItemFactory $cfItemSourceFactory
+    ) {
+        $this->_cfItemSourceFactory = $cfItemSourceFactory;
+        parent::__construct($context, $converter);
+    }
+
     protected function _getOptions()
     {
-        return [0 => 'Test', 1 => 'Test33'];
+        $res = [];
+        /** @var $cfItemSourceOut CfItem */
+        $cfItemSourceOut = $this->_cfItemSourceFactory->create(['cfType' => 'OUT']);
+        foreach ($cfItemSourceOut->getCfItems() as $item) {
+            $res[$item['cf_item_id']] = $item['title'];
+        }
+
+        return $res;
     }
 }
