16482cbb0842fd6ce94a464ef851816300d3b6ed
mihail <tsurkanovm@gmail.com>
2023-03-21 10:06:34 +0200

    draft version of income import model

---
+++ app/code/Tsum/CashFlow/Model/Import/IncomeFormat.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Tsum\CashFlow\Model\Import;
+
+class IncomeFormat
+{
+    public const REGISTRATION_ROW = 0;
+    public const TYPE_ROW = 1;
+    public const CURRENCY_ROW = 2;
+    public const STORAGE_ID_ROW = 3;
+    public const STORAGE_ROW = 4;
+    public const ITEM_ID_ROW = 5;
+    public const ITEM_ROW = 6;
+    public const PROJECT_ROW = 7;
+    public const TOTAL_ROW = 8;
+}
--- app/code/Tsum/CashFlow/Model/Import/Ones.php
+++ app/code/Tsum/CashFlow/Model/Import/Ones.php
@@ -5,29 +5,49 @@ namespace Tsum\CashFlow\Model\Import;
 
 
 
+use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\File\Csv;
 
 class Ones
 {
+    public const SOURCE_FOLDER = 'ones';
+    public const INCOME_FILE_NAME = 'incomes.csv';
+    public const TRANSFER_FILE_NAME = 'transfers.csv';
 
     public function __construct(
-        private readonly Csv $csvReader
+        private readonly Csv $csvReader,
+        private readonly DirectoryList $directoryList
     )
     {
     }
 
     public function importByType(?int $type)
     {
-        return $type ? $this->importTransfers() : $this->importIncomes();
+        $this->csvReader->setDelimiter(';');
+        $filePath = $this->getOnesFilesPath() . self::INCOME_FILE_NAME;
+        $dataFromFile = $this->csvReader->getData($filePath);
+        unset($dataFromFile[0]);
+        foreach ($dataFromFile as $rowData) {
+            $type ? $this->importTransfers($rowData) : $this->importIncomes($rowData);
+        }
     }
 
-    public function importIncomes()
+    public function importIncomes(array $rowData)
     {
-        //$this->csvReader->getData();
+        $storageId = $this->getStorage($rowData);
+        $cfItemId = $this->getCfItem($rowData);
+        $projectId = $this->getProject($rowData);
+        // create income
     }
 
     public function importTransfers()
     {
 
+    }
+
+    private function getOnesFilesPath() : string
+    {
+        return $this->directoryList->getPath(DirectoryList::VAR_DIR) . DIRECTORY_SEPARATOR .
+            self::SOURCE_FOLDER . DIRECTORY_SEPARATOR;
     }
 }
