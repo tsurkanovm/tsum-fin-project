ad00e07f8e1bb691488edca25a024ed3a1c7c7f2
Tsurkanov Mykhailo <tsurkanovm@gmail.com>
2024-01-11 16:23:16 +0200

    remove unused elements from footer, add best-result widget to the footer

--- app/code/Tsum/Digits/Model/ResultRepository.php
+++ app/code/Tsum/Digits/Model/ResultRepository.php
@@ -4,12 +4,12 @@ namespace Tsum\Digits\Model;
 
 use Magento\Framework\Api\FilterBuilder;
 use Magento\Framework\Api\Search\SearchResultFactory;
+use Magento\Framework\Api\Search\SearchResultInterface;
 use Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\Api\SearchCriteriaInterface;
 use Magento\Framework\Api\SortOrderBuilder;
 use Magento\Framework\Exception\CouldNotSaveException;
-use Magento\Framework\Exception\LocalizedException;
 use Tsum\Digits\Api\ResultRepositoryInterface;
 use Tsum\Digits\Api\Data\ResultInterface;
 use Tsum\Digits\Model\ResourceModel\Result as ResultResource;
@@ -19,78 +19,19 @@ use Magento\Framework\Api\Search\SearchCriteriaInterfaceFactory;
 
 class ResultRepository implements ResultRepositoryInterface
 {
-    /**
-     * @var ResultResource
-     */
-    private $resource;
-
-    /**
-     * @var CollectionFactory
-     */
-    private $resultCollectionFactory;
-
-    /**
-     * @var SearchCriteriaInterfaceFactory
-     */
-    private $searchCriteriaInterfaceFactory;
-
-    /**
-     * @var CollectionProcessorInterface
-     */
-    private $collectionProcessor;
-
-    /**
-     * @var SearchCriteriaBuilder
-     */
-    private $searchCriteriaBuilder;
-
-    /**
-     * @var FilterBuilder
-     */
-    private $filterBuilder;
-
-    /**
-     * @var SortOrderBuilder
-     */
-    private $sortOrderBuilder;
-
-    /**
-     * @var SearchResultFactory
-     */
-    private $searchResultsFactory;
-
-    /**
-     * ResultRepository constructor.
-     * @param ResultResource $resource
-     * @param CollectionFactory $resultCollectionFactory
-     * @param SearchCriteriaInterfaceFactory $searchCriteriaInterfaceFactory
-     * @param CollectionProcessorInterface $collectionProcessor
-     * @param SearchCriteriaBuilder $searchCriteriaBuilder
-     * @param FilterBuilder $filterBuilder
-     * @param SortOrderBuilder $sortOrderBuilder
-     * @param SearchResultFactory $searchResultsFactory
-     */
     public function __construct(
-        ResultResource $resource,
-        CollectionFactory $resultCollectionFactory,
-        SearchCriteriaInterfaceFactory $searchCriteriaInterfaceFactory,
-        CollectionProcessorInterface $collectionProcessor,
-        SearchCriteriaBuilder $searchCriteriaBuilder,
-        FilterBuilder $filterBuilder,
-        SortOrderBuilder $sortOrderBuilder,
-        SearchResultFactory $searchResultsFactory
+        private readonly ResultResource $resource,
+        private readonly CollectionFactory $resultCollectionFactory,
+        private readonly SearchCriteriaInterfaceFactory $searchCriteriaInterfaceFactory,
+        private readonly CollectionProcessorInterface $collectionProcessor,
+        private readonly SearchCriteriaBuilder $searchCriteriaBuilder,
+        private readonly FilterBuilder $filterBuilder,
+        private readonly SortOrderBuilder $sortOrderBuilder,
+        private readonly SearchResultFactory $searchResultsFactory
     ) {
-        $this->resource = $resource;
-        $this->resultCollectionFactory = $resultCollectionFactory;
-        $this->searchCriteriaInterfaceFactory = $searchCriteriaInterfaceFactory;
-        $this->collectionProcessor = $collectionProcessor;
-        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
-        $this->filterBuilder = $filterBuilder;
-        $this->sortOrderBuilder = $sortOrderBuilder;
-        $this->searchResultsFactory = $searchResultsFactory;
     }
 
-    public function save($result)
+    public function save(ResultInterface $result): void
     {
         try {
             $this->resource->save($result);
@@ -100,58 +41,47 @@ class ResultRepository implements ResultRepositoryInterface
                 $exception
             );
         }
-
-        return $result;
     }
 
     /**
      * @inheritDoc
      */
-    public function getList(SearchCriteriaInterface $criteria)
+    public function getList(SearchCriteriaInterface $criteria): SearchResultInterface
     {
-        /** @var Collection $collection */
         $collection = $this->resultCollectionFactory->create();
 
         $this->collectionProcessor->process($criteria, $collection);
 
         $searchResults = $this->searchResultsFactory->create();
         $searchResults->setSearchCriteria($criteria);
+        // @phpstan-ignore-next-line
         $searchResults->setItems($collection->getItems());
         $searchResults->setTotalCount($collection->getSize());
 
         return $searchResults;
     }
 
-    public function getThreeVeryBest()
+    public function getThreeVeryBest(): array
     {
-        $result = null;
-
         $hitSortOrder = $this->sortOrderBuilder
             ->setField(ResultInterface::HITS)
-            ->setDescendingDirection()
+            ->setAscendingDirection()
             ->create();
         $timeSortOrder = $this->sortOrderBuilder
             ->setField(ResultInterface::TIME)
-            ->setDescendingDirection()
+            ->setAscendingDirection()
             ->create();
         $this->searchCriteriaBuilder->setSortOrders([$hitSortOrder, $timeSortOrder]);
         $this->searchCriteriaBuilder->setPageSize(3);
 
-        if ($searchRes = $this->getList($this->searchCriteriaBuilder->create())) {
-            $result = json_encode(array_map(
-                function ($item) {
-                    return $item->getData();
-                },
-                $searchRes->getItems()
-            ));
-        }
-
-        return $result;
+        $searchRes = $this->getList($this->searchCriteriaBuilder->create());
+        // @phpstan-ignore-next-line
+        return $searchRes->getItems();
     }
 
-    public function getLastUserResult(string $customerId)
+    public function getLastUserResult(string $customerId): array
     {
-        $result = null;
+        $result = [];
 
         $filterCurrentUser = $this->filterBuilder
             ->setField(ResultInterface::CUSTOMER_ID)
@@ -166,11 +96,10 @@ class ResultRepository implements ResultRepositoryInterface
         $this->searchCriteriaBuilder->addFilters([$filterCurrentUser]);
         $this->searchCriteriaBuilder->setPageSize(1);
 
-        if ($searchRes = $this->getList($this->searchCriteriaBuilder->create())) {
-            /* @var $item ResultInterface */
-            if ($item = current($searchRes->getItems())) {
-                $result = json_encode($item->getData());
-            }
+        $searchRes = $this->getList($this->searchCriteriaBuilder->create());
+        /* @var $item ResultInterface */
+        if ($item = current($searchRes->getItems())) {
+            $result = $item->getData();
         }
 
         return $result;
