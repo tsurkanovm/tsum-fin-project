eda94288665e03e38949d95c59c4f97b5b21d197
Tsurkanov Mykhailo <tsurkanovm@gmail.com>
2024-10-02 09:57:47 +0300

    added Graycore_Cors module to be able to use API in my others JS projects

--- app/etc/config.php
+++ app/etc/config.php
@@ -328,6 +328,7 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 0,
         'DEG_CustomReports' => 1,
+        'Graycore_Cors' => 1,
         'MageSpices_Core' => 0,
         'MageSpices_Mage2Dark' => 0,
         'PayPal_Braintree' => 1,
