633f17d60a27319e4ebbed39b7946e0188ec81b4
Tsurkanov Mykhailo <tsurkanovm@gmail.com>
2024-10-02 10:41:24 +0300

    Removed ignored files

--- .odocker/setup.sql
+++
@@ -1,5 +0,0 @@
-SET @schema = 'fin';
-
-DELETE FROM core_config_data WHERE path LIKE 'catalog/search/elasticsearch7_server_hostname';
-INSERT INTO `core_config_data` (`config_id`, `scope`, `scope_id`, `path`, `value`, `updated_at`) VALUES (NULL, 'default', '0', 'catalog/search/elasticsearch7_server_hostname', 'elasticsearch', current_timestamp());
-INSERT INTO `core_config_data` (`config_id`, `scope`, `scope_id`, `path`, `value`, `updated_at`) VALUES (NULL, 'default', '0', 'catalog/search/engine', 'elasticsearch7', current_timestamp());
